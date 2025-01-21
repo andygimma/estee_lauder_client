e13f73e2de42ca63c041cb218315cad2de730eb0
Andy Gimma <andrewgimma@protonmail.com>
2025-01-21 10:58:57 -0500

    load leaflet map with markers

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
 import "leaflet/dist/leaflet.css";
 
 const queryClient = new QueryClient();
@@ -43,12 +45,35 @@ function Card() {
 }
 
 function Map() {
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "Pop up 1",
+    },
+    {
+      geocode: [48.85, 2.35],
+      popUp: "Pop up 2",
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "Pop up 3",
+    },
+  ];
   return (
     <MapContainer className="h-[100vh]" center={[51.505, -0.09]} zoom={3}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MarkerClusterGroup chunkedLoading>
+        {markers.map((marker) => (
+          <Marker position={marker.geocode}>
+            <Popup>
+              <h2>{marker.popUp}</h2>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 }
